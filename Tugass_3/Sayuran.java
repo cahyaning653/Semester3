36df6c542220efbf9cb655f707d09f7361f33587
cahyaning653 <162890134+cahyaning653@users.noreply.github.com>
2025-01-08 15:51:39 +0700

    Add files via upload

---
+++ Tugass_3/Sayuran.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugass_3;
+class Sayuran extends Makanan {
+    boolean organik;
+    
+    void dimasak(){
+        System.out.println("Sayuran dimasak");
+    }
+    
+}
---
+++ Tugass_3/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugass_3;
+public class main {
+    public static void main(String[] args) {
+        Makanan enak = new Makanan ();
+        enak.dikonsumsi();
+        Buah manis = new Buah ();
+        manis.dikupas();
+        Sayuran segar = new Sayuran ();
+        segar.dimasak();    
+    }
+}
